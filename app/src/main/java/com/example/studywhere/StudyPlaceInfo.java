608aef91b381f3f46508f3024a30eedc1f186bdd
Winfred Huang <whuang@ucsb.edu>
2019-05-17 00:40:10 -0700

    Merged Conflicted work. Added new activities. ->PickMapsActivity.java ->StudyPlaceInfo.java ->SuggestNewPlaces.java

---
+++ app/src/main/java/com/example/studywhere/StudyPlaceInfo.java
@@ -0,0 +1,13 @@
+package com.example.studywhere;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StudyPlaceInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_study_place_info);
+    }
+}
