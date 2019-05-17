8acc24149536ae5f0d706000e1aecdafd2548986
jacobazamora <jacob.zamora524@gmail.com>
2019-05-17 00:29:22 -0700

    made input screen

---
+++ app/src/main/java/com/example/studywhere/MakeGroup.java
@@ -0,0 +1,13 @@
+package com.example.studywhere;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MakeGroup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_group);
+    }
+}
