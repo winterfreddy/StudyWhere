6cf999fb93c60f99d3578f40cf1f7a5b324b0822
Winfred Huang <whuang@ucsb.edu>
2019-05-29 22:39:25 -0700

    Removed/Modified files. Addition of Helper screen. Continuation of redesigning interface.

--- app/src/main/java/com/example/studywhere/HelpScreen.java
+++ app/src/main/java/com/example/studywhere/HelpScreen.java
@@ -3,11 +3,11 @@ package com.example.studywhere;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class HelpScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_help_screen);
     }
 }
--- app/src/main/java/com/example/studywhere/ViewMapOnly.java
+++ app/src/main/java/com/example/studywhere/ViewMapOnly.java
@@ -1,7 +1,10 @@
 package com.example.studywhere;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -22,6 +25,16 @@ public class ViewMapOnly extends FragmentActivity implements OnMapReadyCallback 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        final Button mhelpbutton = findViewById(R.id.helpbutton);
+        mhelpbutton.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent( "studywhere.HelpScreen");
+                startActivity(intent);
+            }
+        }
+        );
     }
 
 
